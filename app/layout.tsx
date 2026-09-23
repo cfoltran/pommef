232129ccd156bf4396e6c053bdb58f0d69706a12
cfoltran <clement.foltran@protonmail.com>
2026-09-23 11:42:53 +0200

    Update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/pommef_logo-05.svg" type="image/svg+xml" />
-      </head>
       <body className="bg-white text-black">{children}</body>
     </html>
   );
